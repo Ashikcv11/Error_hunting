39835df44a1bf69b91eaed5dad48c07f60e0b8e7
ashik <ashikcv17@gmail.com>
2022-05-06 21:08:48 -0400

    error_hunting

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express'); // express was not installed
+const path = require ('path'); 
+const cors = require('cors');
+var bodyParser = require('body-parser') // required bodyparser 
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homeRouter'); //4 spelling mistake in the given path
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+const app = new express; 
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+// app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("Server Ready on 3000");
+});
---
+++ src/data/user.js
@@ -0,0 +1,23 @@
+var users=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+},
+{
+    "id":4,
+    "uid":"ashikcv@gmail.com",
+    "pwd":"55555"
+}]
+
+
+module.exports=users; // "user" change to "users"
